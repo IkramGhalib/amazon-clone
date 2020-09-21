232350919face22bf6c03fb04c58aa703a936625
IkramGhalib <39878945+IkramGhalib@users.noreply.github.com>
2020-09-21 15:13:14 +0500

    login authentication

--- src/Login.js
+++ src/Login.js
@@ -1,46 +1,84 @@
 import React, { useState } from "react";
 import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const signIn = (e) => {
-    //   prevent refershing react page
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
+
   const register = (e) => {
     e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
   };
+
   return (
     <div className="login">
-      <img
-        className="login__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
       <div className="login__container">
-        <h1>Sign in</h1>
+        <h1>Sign-in</h1>
+
         <form>
-          <h5>Email</h5>
+          <h5>E-mail</h5>
           <input
             type="text"
-            value={Email}
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+
           <h5>Password</h5>
           <input
             type="password"
-            value={Password}
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
 
-          <button onClick={signIn} className="login__button">
-            Login
-          </button>
-          <button onClick={register} className="login__button">
-            Create Account
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
           </button>
         </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,23 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyALUDCfIRbCt_2INWHFV7_kyRSDs1O5L4A",
-    authDomain: "clone-3794b.firebaseapp.com",
-    databaseURL: "https://clone-3794b.firebaseio.com",
-    projectId: "clone-3794b",
-    storageBucket: "clone-3794b.appspot.com",
-    messagingSenderId: "261505085872",
-    appId: "1:261505085872:web:115377ac49e4717dd4a675",
-    measurementId: "G-GJ65NVK0P0"
-  };
+  apiKey: "AIzaSyALUDCfIRbCt_2INWHFV7_kyRSDs1O5L4A",
+  authDomain: "clone-3794b.firebaseapp.com",
+  databaseURL: "https://clone-3794b.firebaseio.com",
+  projectId: "clone-3794b",
+  storageBucket: "clone-3794b.appspot.com",
+  messagingSenderId: "261505085872",
+  appId: "1:261505085872:web:115377ac49e4717dd4a675",
+  measurementId: "G-GJ65NVK0P0",
+};
+// const firebaseApp = firebase.initializedApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// export { db, auth };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
