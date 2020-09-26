d30c215c54ae3e00b44154473c8e00b791e187fc
IkramGhalib <39878945+IkramGhalib@users.noreply.github.com>
2020-09-26 11:36:43 +0500

    api routes

--- functions/index.js
+++ functions/index.js
@@ -3,3 +3,8 @@ const express = require("express");
 const { queries } = require("@testing-library/react");
 const cors = requries("core");
 const stripe = require("stripe")("sk_test_WIPLVI7Y0Qxu0zKi4YclU6tX0042P9Lzco");
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+// app.get('/')
+app.get("/", (request, response) => response.status(200).send("hello world"));
